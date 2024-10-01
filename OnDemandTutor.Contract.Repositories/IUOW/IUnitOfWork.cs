35b4125378691517b7fb91d8cd48e8c4b1a7946d
ThuanDeepTry <nguyenminhthuan12a8tp1@gmail.com>
2024-10-01 13:08:08 +0700

    Update IUnitOfWork.cs

--- OnDemandTutor.Contract.Repositories/IUOW/IUnitOfWork.cs
+++ OnDemandTutor.Contract.Repositories/IUOW/IUnitOfWork.cs
@@ -6,12 +6,7 @@ namespace OnDemandTutor.Contract.Repositories.Interface
 {
     public interface IUnitOfWork : IDisposable
     {
-        IGenericRepository<T> GetRepository<T>() where T : class; // Phương thức lấy repository chung cho mọi loại thực thể
-        IGenericRepository<Schedule> ScheduleRepository { get; } // Repository cho Schedule
-        IGenericRepository<TutorSubject> TutorRepository { get; } // Repository cho TutorSubject
-        IGenericRepository<Subject> SubjectRepository { get; } // Repository cho Subject
-        IGenericRepository<Feedback> FeedbackRepository { get; } // Repository cho Feedback
-        IGenericRepository<Complaint> ComplaintRepository { get; } // Đã sửa từ ComplaintRpository sang ComplaintRepository
+       
         IGenericRepository<T> GetRepository<T>() where T : class;
         IGenericRepository<Schedule> ScheduleRepository { get; }
         IGenericRepository<TutorSubject> TutorRepository { get; }
@@ -26,10 +21,6 @@ namespace OnDemandTutor.Contract.Repositories.Interface
         void CommitTransaction();
         void RollBack();
 
-        void Save(); // Lưu thay đổi
-        Task SaveAsync(); // Lưu thay đổi không đồng bộ
-        void BeginTransaction(); // Bắt đầu giao dịch
-        void CommitTransaction(); // Cam kết giao dịch
-        void RollBack(); // Hoàn tác giao dịch
+     
     }
 }
